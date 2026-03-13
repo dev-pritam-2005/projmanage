bb5457e6e5ec2aa1f9815dece9448ee7fc0ae39a
Pritam Dutta <Pritamdutta35689@gmail.com>
2026-03-13 18:55:40 +0530

    async handler and bcrypt added

--- src/controllers/healthcheck.controllers.js
+++ src/controllers/healthcheck.controllers.js
@@ -1,14 +1,24 @@
 import { ApiResponse } from "../utils/api-response.js";
+import { asyncHandler } from "../utils/async-handler.js";
 
-const healthCheck = (req, res) => {
-    try {
-        return res.status(200).json(
-            new ApiResponse(200, { message: "server is running" }, "Health check success")
-        );
-    } catch (error) {
-        console.log(error);
-    }
-};
+// const healthCheck = async(req, res,next) => {
+//     try {
+//         const user = await getUserFromDb()
+//         return res
+//                 .status(200)
+//                 .json(new ApiResponse(200, { message: "server is running" }, "Health check success")
+//         );
+//     } catch (error) {
+//         console.log(error);
+//         next(err)
+//     }
+// };
+const healthCheck = asyncHandler(async (req, res) => {
+  res
+  .status(200)
+  .json(new ApiResponse(200, { message: "Server is still running" }));
+});
+
 
 export default healthCheck;
 
