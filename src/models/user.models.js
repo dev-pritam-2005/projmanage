2ddcb32a9c826aa24eac07075f6f4b57e3db90d9
Pritam Dutta <Pritamdutta35689@gmail.com>
2026-03-16 03:59:19 +0530

    Auth middleware added

--- src/models/user.models.js
+++ src/models/user.models.js
@@ -74,28 +74,26 @@ userSchema.methods.isPasswordCorrect= async function(password) {
     return await bcrypt.compare(password,this.password)
 }
 
-userSchema.methods.generateAccessToken = function(){
-      jwt.sign(
-      {
-        _id: this._id,
-        email: this.email,
-        username:this.username
-       },
-      process.env.ACCESS_TOKEN_SECRET,
-      { expiresIn: process.env.ACCESS_TOKEN_EXPIRY }
-)
-}
-
-userSchema.methods.generateRefreshToken = function(){
-      jwt.sign(
-      {
-        _id: this._id,
-        
-       },
-      process.env.REFRESH_TOKEN_SECREAT,
-      { expiresIn: process.env.REFRESH_TOKEN_EXPIRY }
-)
-}
+userSchema.methods.generateAccessToken = function () {
+  return jwt.sign(
+    {
+      _id: this._id,
+      email: this.email,
+      username: this.username
+    },
+    process.env.ACCESS_TOKEN_SECRET,
+    { expiresIn: process.env.ACCESS_TOKEN_EXPIRY }
+  );
+};
+userSchema.methods.generateRefreshToken = function () {
+  return jwt.sign(
+    {
+      _id: this._id
+    },
+    process.env.REFRESH_TOKEN_SECRET,
+    { expiresIn: process.env.REFRESH_TOKEN_EXPIRY }
+  );
+};
 
 userSchema.methods.generateTemporaryToken = function () {
     const unHashedToken = crypto.randomBytes(20).toString("hex")
