ac7d2e310234e6d6990b26ea5042c6a68ffc28ee
Pritam Dutta <Pritamdutta35689@gmail.com>
2026-03-31 15:39:16 +0530

    chnages on app.js

--- src/app.js
+++ src/app.js
@@ -26,9 +26,12 @@ app.use(cors({
 
 import healthCheckRouter from "./routes/healthcheck.routes.js";
 import authRouter from "./routes/auth.routes.js"
+import projectRouter from "./routes/project.routes.js"
 
 app.use("/api/v1/healthcheck",healthCheckRouter)
 app.use("/api/v1/authRouter",authRouter)
+app.use("/api/v1/projectRouter",projectRouter)
+
 
 
 app.get("/",(req,res)=>{
